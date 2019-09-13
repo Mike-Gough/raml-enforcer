baadb649022b129eaa6d327f30fafd1da4a57d77
Mike <mrg14r@gmail.com>
2019-09-13 16:09:16 +1000

    work in progress

--- raml-enforcer.js
+++ raml-enforcer.js
@@ -3,15 +3,11 @@
 
 var validationOptions = require('./validation-options.json') // linter configuration
 var pkg = require('./package.json') // NPM package configuration
-var raml = require('raml-1-parser') // RAML parser
+var raml = require('webapi-parser').WebApiParser // RAML parser
 var commander = require('commander') // Command line argument parser
 var colors = require('colors') // Colors and styles for console
 var path = require('path') // node.js library for working with files and directories
 var _ = require('lodash') // lodash library
-var kindEnum = Object.freeze({
-    "error": 0,
-    "warning": 1
-  })
 
 // Parse command line arguments
 commander
@@ -42,30 +38,19 @@ _.forEach(validationOptions, function(value, key) {
 
 console.log('report:'.bold)
 
-// Process each file sequentially
 _.forEach(commander.args, (filePath) => {
-  raml.loadRAML(filePath)
-    .catch((error) => {
-      error = [{
-        src: filePath,
-        message: error.message,
-        kind: kindEnum.error
-      }]
-      throw error
+  raml.raml10.parse(`file://${filePath}`)
+    .then((webApiBaseUnit) => {
+      return raml.raml10.validate(webApiBaseUnit)
     })
-    .then((ramlContent) => {
-
-      // Check ramlContent for parser issues
+    .then((resultPromise) => {
+      // Get parser issues
       var issues = _.flatten(
-        ramlContent.errors().map((issue) => {
-          var name = path.join(path.dirname(filePath), issue.path)
-
-          if ((validationOptions.includes && name == filePath) || (issue.isWarning && validationOptions.warnings) || (!issue.isWarning && validationOptions.errors)) {
-            return {
-              src: name + ':' + issue.range.start.line + ':' + issue.range.start.column,
-              message: issue.message,
-              kind: issue.isWarning ? kindEnum.warning : kindEnum.error
-            }
+        resultPromise.results.map((issue) => {
+          return {
+            src: issue.location + ': ' + issue.position,
+            message: issue.message,
+            kind: issue.level
           }
         })
       )
@@ -73,25 +58,16 @@ _.forEach(commander.args, (filePath) => {
       var issueCountByKind = _.countBy(issues, function(issue) {
         return issue.kind;
       })
-
+      
       // If the RAML file is valid, check for style and quality issues
-      if (_.isEmpty(issues) && [kindEnum.error] == 0) {
-
-        // Validate that the latest version of RAML is used
-        if (validationOptions.warnOldRamlVersion && (ramlContent.RAMLVersion() != 'RAML10')) {
-          issues.push({
-            src: filePath,
-            message: 'RAML should be upgraded to version 1.0',
-            kind: kindEnum.warning
-          })
-        }
+      if (_.isEmpty(issues)) {
 
         // Validate that the API has a title
         if ((_.isEmpty(ramlContent.title()))) {
           issues.push({
             src: filePath,
             message: 'API should spesify a title property',
-            kind: kindEnum.warning
+            kind: 'Warning'
           })
         }
 
@@ -101,7 +77,7 @@ _.forEach(commander.args, (filePath) => {
             issues.push({
               src: filePath,
               message: 'Resource should have a description ' + location,
-              kind: kindEnum.warning
+              kind: 'Warning'
             })
           }
           _.each(resource.resources(), function(childResource) {
@@ -127,12 +103,14 @@ _.forEach(commander.args, (filePath) => {
       console.log('  ' + colors.white('[' + result.src + ']') + ' ' + colors.green(result.message))
     })
     .catch((error) => {
+
+      console.log(error)
       error.forEach((issue) => {
         switch (issue.kind) {
-          case kindEnum.warning:
+          case "Warning":
             console.log('  ' + colors.white('[' + issue.src + ']') + ' ' + colors.yellow('WARN') + ' ' + colors.yellow(issue.message))
             break;
-          case kindEnum.error:
+          case "Violation":
             console.log('  ' + colors.white('[' + issue.src + ']') + ' ' + colors.red('ERROR') + ' ' + colors.red(issue.message))
             break;
         }
@@ -143,7 +121,7 @@ _.forEach(commander.args, (filePath) => {
       })
 
       if ((issueCountByKind[kindEnum.warning] != undefined & issueCountByKind[kindEnum.warning] > 0 & validationOptions.throwOnWarnings) ||
-         (issueCountByKind[kindEnum.error] != undefined & issueCountByKind[kindEnum.error] > 0 & validationOptions.throwOnErrors)) {
+         (issueCountByKind[kindEnum.Violation] != undefined & issueCountByKind[kindEnum.error] > 0 & validationOptions.throwOnErrors)) {
           console.log('Exiting with code 1')
           process.exit(1)
       } else {
